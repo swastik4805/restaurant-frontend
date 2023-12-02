7e4311cb8699563bf7b53b364b3019dd80a0740b
SWASTIK CHATTERJEE <chatterjeeswastik7772@gmail.com>
2023-12-02 21:02:37 +0530

    added spaces between social buttons

--- src/components/RestaurantDetails.js
+++ src/components/RestaurantDetails.js
@@ -8,6 +8,29 @@ const RestaurantDetails = () => {
   const { name } = useParams();
   const [restaurant, setRestaurant] = useState(null);
 
+  const handleAddressClick = () => {
+    // Open Google Maps in a new tab with the restaurant's location
+    if (restaurant && restaurant.location) {
+      const { latitude, longitude, address } = restaurant.location;
+      const mapsUrl = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
+      window.open(mapsUrl, '_blank');
+    }
+  };
+
+  const handleWebsite=()=>{
+    window.open(restaurant.online_presence.social_media.website,'_blank');
+  }
+  const handleFacebook=()=>{
+    window.open(restaurant.online_presence.social_media.facebook,'_blank');
+  }
+  const handleInstagram=()=>{
+    window.open(restaurant.online_presence.social_media.instagram,'_blank');
+  }
+  const handleTwitter=()=>{
+    window.open(restaurant.online_presence.social_media.twitter,'_blank');
+  }
+
+
   useEffect(() => {
     axios.get(`http://localhost:3001/restaurant/${name}`)
       .then((response) => {
@@ -26,6 +49,7 @@ const RestaurantDetails = () => {
     <div className={styles.container}>
     <h2 className={styles.heading}>{restaurant.name}</h2>
       <p><strong>Location:</strong> {restaurant.location.address}</p>
+      <button onClick={handleAddressClick}>checkout location</button>
 
       <h3 className={styles.subHeading}>Chef Details</h3>
       <p><strong>Chef:</strong> {restaurant.chef.name}</p>
@@ -43,10 +67,12 @@ const RestaurantDetails = () => {
     <p>{restaurant.ambiance.description}</p>
 
     <h3>Online Presence</h3>
-    <p>Website: <a href={restaurant.online_presence.website} target="_blank" rel="noopener noreferrer">{restaurant.online_presence.website}</a></p>
-    <p>Facebook: <a href={restaurant.online_presence.social_media.facebook} target="_blank" rel="noopener noreferrer">{restaurant.online_presence.social_media.facebook}</a></p>
-    <p>Instagram: <a href={restaurant.online_presence.social_media.instagram} target="_blank" rel="noopener noreferrer">{restaurant.online_presence.social_media.instagram}</a></p>
-    <p>Twitter: <a href={restaurant.online_presence.social_media.twitter} target="_blank" rel="noopener noreferrer">{restaurant.online_presence.social_media.twitter}</a></p>
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <button onClick={handleWebsite}>website</button>
+        <button onClick={handleFacebook}>Facebook</button>
+        <button onClick={handleInstagram}>Instagram</button>
+        <button onClick={handleTwitter}>Twitter</button>
+    </div>
 
     {/* Add more details based on your requirements */}
   </div>
